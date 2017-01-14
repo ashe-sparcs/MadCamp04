8fb7010a0adf1fe028463446e20e6d68721b82df
ashe-sparcs <wotjd1102@naver.com>
2017-01-14 19:15:51 +0900

    register form validation complete.

--- LiberalOverflow/views.py
+++ LiberalOverflow/views.py
@@ -13,5 +13,6 @@ def register(request):
 def register_complete(request):
     # 인증 메일 보내기
     print(request.method)
+    print(request.POST.get('nickname'))
     # 완료 페이지 띄우기
     return render(request, 'register_complete.html')
