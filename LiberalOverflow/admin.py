a5108cf8aa8e37a87fcb02f55a770eae1ae907cb
guldam <guldam@kaist.ac.kr>
2017-01-17 21:27:43 +0900

    selected cell complete

--- LiberalOverflow/admin.py
+++ LiberalOverflow/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from LiberalOverflow.models import  UserProfile, TimeSlot, Lecture
+
+admin.site.register(UserProfile)
+admin.site.register(TimeSlot)
+admin.site.register(Lecture)
+
+
 # Register your models here.
